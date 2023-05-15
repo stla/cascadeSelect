d0f179902eadb4006d6577808641a13054e0a1f2
Stéphane Laurent <laurent_step@yahoo.fr>
2023-05-15 14:05:52 +0200

    Icon, cssColors

--- R/cascadeSelect.R
+++ R/cascadeSelect.R
@@ -1,6 +1,36 @@
-#' Add Title
+#' @title Create an icon
+#' @description Create an icon with a given color and a given size.
 #'
-#' Add Description
+#' @param icon the name (actually the class name) of the icon;
+#'   \strong{PrimeIcons} are available, for example \code{"pi pi-youtube"} (see
+#'   \href{https://primereact.org/icons/#list}{PrimeIcons}) and
+#'   \strong{fontawesome} icons are available, for example
+#'   \code{"fa fa-paper-plane"}
+#' @param color a CSS color
+#' @param size size of the icon, a css measurement
+#'
+#' @return A list to be included in the field \code{icon} of the \code{choices}
+#'   list of \code{\link{cascadeSelectInput}}.
+#' @export
+Icon <- function(icon, color = "red", size = "1rem") {
+  list(
+    "icon"  = icon,
+    "color" = validateColor(color),
+    "size"  = validateCssUnit(size))
+}
+
+#' @title Cascade select input
+#' @description Create a cascade select input for Shiny.
+#'
+#' @param inputId the id that will be used to get the selected value in Shiny
+#' @param choices a hierarchical list; see the examples
+#' @param selected the selected value; \code{NULL} for none
+#' @param placeholder placeholder appearing when no selected value
+#' @param optionLabel the label of the options to be selected
+#' @param optionGroupLabel the label of the groups of options
+#' @param optionGroupChildren a list of the names of the groups of options
+#'
+#' @return A \code{shiny.tag.list} object to be included in a Shiny UI.
 #'
 #' @importFrom reactR createReactShinyInput
 #' @importFrom htmltools htmlDependency tags
@@ -27,11 +57,11 @@ cascadeSelectInput <- function(
     ),
     default = selected,
     configuration = list(
-      data = choices,
-      placeholder = placeholder,
-      optionLabel = optionLabel,
-      optionGroupLabel = optionGroupLabel,
-      optionGroupChildren = optionGroupChildren
+      "data" = choices,
+      "placeholder" = placeholder,
+      "optionLabel" = optionLabel,
+      "optionGroupLabel" = optionGroupLabel,
+      "optionGroupChildren" = optionGroupChildren
     ),
     container = tags$div
   )
---
+++ R/data-primeicons.R
@@ -0,0 +1,11 @@
+#' @title The prime icons
+#'
+#' @description The names of the icons available in the \strong{PrimeIcons}
+#'   library.
+#'
+#' @format A vector with 260 elements. Each element is the name of an icon.
+#'   In order to use it in the \code{\link{Icon}} function, you have to prefix
+#'   it with \code{pi pi-} (for example \code{"pi pi-youtube"}). See
+#'   \href{https://primereact.org/icons/#list}{PrimeIcons} for the list of
+#'   all icons.
+"pi_icons"
