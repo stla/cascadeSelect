f0e42356b56a924c52be57916ec59987f2847287
Stéphane Laurent <laurent_step@yahoo.fr>
2023-05-19 09:06:14 +0200

    updated themes

--- R/data-themes.R
+++ R/data-themes.R
@@ -3,7 +3,7 @@
 #' @description The names of the CSS themes available in the \strong{PrimeReact}
 #'   library.
 #'
-#' @format A vector with 48 elements. Each element is the name of a theme,
+#' @format A vector with 34 elements. Each element is the name of a theme,
 #'   to be used as the \code{theme} argument of the
 #'   \code{\link{cascadeSelectInput}} function.
 "themes"
